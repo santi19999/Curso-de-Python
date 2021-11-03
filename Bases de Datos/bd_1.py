44b89764b78ab71da0b90a26f6da5c199f73e417
santi19999 <santiherrera1999@gmail.com>
2021-11-02 22:32:38 -0300

    Conexion a Db Y Creación Tabla

---
+++ Bases de Datos/bd_1.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+
+
+miConexion = sqlite3.connect("PrimeraBase")
+miCursor = miConexion.cursor()
+
+miCursor.execute("CREATE TABLE PRODUCTOS ,(id INTEGER PRIMARY, nombre VARCHAR (100), apellido VARCHAR(100))")
