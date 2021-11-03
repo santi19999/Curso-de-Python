7a890b0a640a5700552e9811d636347950c3c5f5
santi19999 <santiherrera1999@gmail.com>
2021-11-03 13:13:24 -0300

    FuncionFIlter

--- Funciones Filter/filter_1.py
+++ Funciones Filter/filter_1.py
@@ -1,22 +1,23 @@
-import sqlite3
+#SE UTILIZA GENERALMENTE PARA FILTRAR OBJETOS
+class Empleado:
+    def __init__(self,nombre,cargo,salario):
+        self.nombre = nombre
+        self.cargo = cargo
+        self.salario = salario
+    
+    def __str__(self):
+        return "{} que trabaja como {} tiene un salario de $$ {}".format(self.nombre,self.cargo,self.salario)
 
 
 
-def new_register(datos):
-    miCursor.executemany("INSERT INTO REGISTROS VALUE(?,?)",datos)
-
-def select_datos():
-    miCursor.execute("SELECT * FROM REGISTROS WHERE EMAIL=")
-
-
-miConexion = sqlite3.connect("DB_DATOS")
-miCursor = miConexion.cursor()
-
-miCursor.execute("CREATE TABLE REGISTROS IF NOT EXISTS (NOMBRE VARCHAR UNIQUE(50), EMAIL VARCHAR UNIQUE (50))")
-datos = [
-    ("Sergio Herrera","sergiodanielherrera23@gmail.com"),
-    ("Santiago Herrera","santiherrera1999@gmail.com"),
-    ("Martina Herrera"," martinaherrera@gmail.com")
-
+listaEmpleados = [
+    Empleado("Juan","Director",75000),
+    Empleado("Ana","Presidenta",85000),
+    Empleado("Roberto","Administrativo",25000),
+    Empleado("Sara","Secretaria",27000),
+    Empleado("Mario","Botones",21000)
 ]
-new_register(datos)
+salarios_altos = filter(lambda empleado:empleado.salario>50000,listaEmpleados)
+
+for empleado_salario in salarios_altos:
+    print(empleado_salario)
