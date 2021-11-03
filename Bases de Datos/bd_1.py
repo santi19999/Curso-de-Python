7321ea9132e8519cbff9c509b7cf60b8f95cc1fc
santi19999 <santiherrera1999@gmail.com>
2021-11-03 00:03:54 -0300

    leemos los datos y los mostramos

--- Bases de Datos/bd_1.py
+++ Bases de Datos/bd_1.py
@@ -6,7 +6,16 @@ miConexion = sqlite3.connect("PrimeraBase")
 miCursor = miConexion.cursor()
 
 miCursor.execute("CREATE TABLE IF NOT EXISTS PRODUCTOS (id INTEGER (20) , nombre VARCHAR (100), apellido VARCHAR(100))")
-miCursor.execute("INSERT INTO PRODUCTOS VALUES(1,'sERGIO','hERRERA')")
 
-miConexion.commit()
+misDatos=[
+    (3,"Noelia","Gauna"),
+    (4,"Sergio","Gauna"),
+    (5,"Denis","Gauna"),
+    (6,"Martina","Herrera Gauna")
+]
+
+#Seleccionamos todos los datos de la tabla y los almacenamos en una lista
+miCursor.execute("SELECT * FROM PRODUCTOS")
+misDatos = miCursor.fetchall()
+print(misDatos)
 miConexion.close()
