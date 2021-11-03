cbc62647f957c701f1a3aa488b4a2cb2d5a5be74
santi19999 <santiherrera1999@gmail.com>
2021-11-02 23:37:43 -0300

    Creacion,Conexion,insercion de campo db

--- Bases de Datos/bd_1.py
+++ Bases de Datos/bd_1.py
@@ -5,6 +5,8 @@ import sqlite3
 miConexion = sqlite3.connect("PrimeraBase")
 miCursor = miConexion.cursor()
 
-'''miCursor.execute("CREATE TABLE PRODUCTOS ,(id INTEGER PRIMARY, nombre VARCHAR (100), apellido VARCHAR(100))")
-miConexion.commit()'''
+miCursor.execute("CREATE TABLE IF NOT EXISTS PRODUCTOS (id INTEGER (20) , nombre VARCHAR (100), apellido VARCHAR(100))")
+miCursor.execute("INSERT INTO PRODUCTOS VALUES(1,'sERGIO','hERRERA')")
+
+miConexion.commit()
 miConexion.close()
